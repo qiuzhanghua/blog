6eb8009ddf1dd91c3307620db91789957ad41224
qiuzhanghua <qiuzhanghua@icloud.com>
2022-07-06 16:48:59 +0800

    initial

---
+++ README.md
@@ -0,0 +1,45 @@
+# NestJS + TypeORM
+
+## Installation
+
+```bash
+pnpm install
+```
+
+## Running the app
+
+```bash
+# development
+pnpm start
+
+# watch mode
+pnpm start:dev
+
+# production mode
+pnpm start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+pnpm test
+
+# e2e tests
+pnpm test:e2e
+
+# test coverage
+pnpm test:cov
+```
+
+## My dotenv
+```dotenv
+DB_TYPE=postgres
+DB_PORT=5432
+#DB_TYPE=mysql
+#DB_PORT=3306
+DB_HOST=localhost
+DB_USERNAME=app
+DB_PASSWORD=app
+DB_NAME=app
+```
---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as "mysql" | "postgres" | "sqlite",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
